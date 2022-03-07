0c56ab32308578984b689eaed67ae156ca34c2e5
ltempe <louistt3@gmail.com>
2022-03-07 13:46:04 +0100

    mongodb insert

---
+++ server/mongodb/index.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+let products = [];
+
+const { MongoClient } = require("mongodb");
+const MONGODB_URI =
+  "mongodb+srv://ltempe:ltempe@clearfashion.4nii2.mongodb.net?retryWrites=true&w=majority";
+const MONGODB_DB_NAME = "clearfashion";
+let client;
+let db;
+let collectionProducts;
+
+const connect = (module.exports.connect = async () => {
+  if (db) console.log("Already connected");
+  else {
+    const client = await MongoClient.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+    });
+    db = await client.db(MONGODB_DB_NAME);
+    collectionProducts = await db.collection("products");
+    console.log("Connection success");
+  }
+});
+
+const insert = (module.exports.insert = async (products) => {
+  try {
+    await connect();
+    await collectionProducts.deleteMany();
+    const results = await collectionProducts.insertMany(products, {
+      ordered: false,
+    });
+    console.log("Everything was insered");
+    return results;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+});
+
+const close = (module.exports.close = async () => {
+  await client.close();
+  console.log("Connection closed");
+});
+
+const find = (module.exports.find = async (query) => {
+  try {
+    await connect();
+    const result = await collectionProducts.find(query).toArray();
+    console.log(result.length);
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//console.log(require("../products/dedicated.json") + );
+for (const file of fs.readdirSync("./products"))
+  products = products.concat(require("../products/" + file));
+
+const test = async () => {
+  await insert(products);
+  await find({ brand: "montlimart" });
+};
+
+test();
--- server/sites/dedicatedbrand.js
+++ server/sites/dedicatedbrand.js
@@ -1,38 +1,32 @@
-const fetch = require('node-fetch');
-const cheerio = require('cheerio');
-const {'v5': uuidv5} = require('uuid');
+const fetch = require("node-fetch");
+const cheerio = require("cheerio");
+const { v5: uuidv5 } = require("uuid");
 
 /**
  * Parse webpage restaurant
  * @param  {String} data - html response
  * @return {Object} restaurant
  */
-const parse = data => {
+const parse = (data) => {
   const $ = cheerio.load(data);
 
-  return $('.productList-container .productList')
+  return $(".productList-container .productList")
     .map((i, element) => {
       const link = `https://www.dedicatedbrand.com${$(element)
-        .find('.productList-link')
-        .attr('href')}`;
+        .find(".productList-link")
+        .attr("href")}`;
 
       return {
         link,
-        'brand': 'dedicated',
-        'price': parseInt(
-          $(element)
-            .find('.productList-price')
-            .text()
-        ),
-        'name': $(element)
-          .find('.productList-title')
+        brand: "dedicated",
+        price: parseFloat($(element).find(".productList-price").text()),
+        name: $(element)
+          .find(".productList-title")
           .text()
           .trim()
-          .replace(/\s/g, ' '),
-        'photo': $(element)
-          .find('.productList-image img')
-          .attr('src'),
-        '_id': uuidv5(link, uuidv5.URL)
+          .replace(/\s/g, " "),
+        photo: $(element).find(".productList-image img").attr("data-src"), //[0].attribs["data-src"],
+        _id: uuidv5(link, uuidv5.URL),
       };
     })
     .get();
@@ -43,13 +37,12 @@ const parse = data => {
  * @param  {[type]}  url
  * @return {Array|null}
  */
-module.exports.scrape = async url => {
+module.exports.scrape = async (url) => {
   try {
     const response = await fetch(url);
 
     if (response.ok) {
       const body = await response.text();
-
       return parse(body);
     }
 
